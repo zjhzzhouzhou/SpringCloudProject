7b9bd45d7e7ca5fbbca1fa02d29ed1423d873678
zhouzhou_sun <13588069316@163.com>
2020-01-06 09:29:46 +0800

    简化redis

--- common/common.cache/src/main/java/com/dyhospital/cloudhis/common/redis/config/RedisConfig.java
+++ common/common.cache/src/main/java/com/dyhospital/cloudhis/common/redis/config/RedisConfig.java
@@ -12,7 +12,6 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.net.UnknownHostException;
 
 /**
  * 
